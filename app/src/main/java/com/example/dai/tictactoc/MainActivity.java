3dc17fe6e6636f4dc9cab974a3df60e4f19724fb
Zoe Dai <zoedai91@gmail.com>
2015-10-11 21:39:07 -0700

    img

--- app/src/main/java/com/example/dai/tictactoc/MainActivity.java
+++ app/src/main/java/com/example/dai/tictactoc/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.dai.tictactoc;
 
-import android.content.DialogInterface;
-import android.media.Image;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -25,15 +25,34 @@ public class MainActivity extends AppCompatActivity {
 
         gameTable = (TableLayout) findViewById(R.id.gameTable);
 
+        View.OnClickListener listener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = getResources().getResourceName(v.getId());
+                int ind = name.charAt(name.length() - 1) - '1';
+                int i = ind / 3;
+                int j = ind % 3;
+                if (game[i][j] != 0) {
+                    return;
+                }
+
+                game[i][j] = player ? 2 : 1;
+                buttons[i][j].setImageResource(player ? R.drawable.o : R.drawable.x);
+
+                player = !player;
+            }
+        };
+
         for (int i = 0; i < 3; i++) {
             TableRow row = (TableRow) gameTable.getChildAt(i);
             for (int j = 0; j < 3; j++) {
                 buttons[i][j] = (ImageButton) row.getChildAt(j);
                 buttons[i][j].setImageResource(R.drawable.empty);
+                buttons[i][j].setScaleType(ImageView.ScaleType.FIT_XY);
+                buttons[i][j].setAdjustViewBounds(true);
+                buttons[i][j].setOnClickListener(listener);
             }
         }
-
-        
 
 
 
