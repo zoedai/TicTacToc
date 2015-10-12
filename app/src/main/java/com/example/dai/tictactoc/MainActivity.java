4aaa42decc5cdc81c67cc214c0ece22b38db9ce5
Zoe Dai <zoedai91@gmail.com>
2015-10-11 23:58:01 -0700

    final

--- app/src/main/java/com/example/dai/tictactoc/MainActivity.java
+++ app/src/main/java/com/example/dai/tictactoc/MainActivity.java
@@ -1,33 +1,48 @@
 package com.example.dai.tictactoc;
 
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TableLayout gameTable;
     ImageButton[][] buttons = new ImageButton[3][3];
+    TextView status;
     int[][] game = new int[3][3];
-    boolean player;
+    int totalSteps = 0;
+    Button resetBtn;
+    boolean gameOngoing = true;
+    int player = 1;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        status = (TextView) findViewById(R.id.status);
         gameTable = (TableLayout) findViewById(R.id.gameTable);
+        resetBtn = (Button) findViewById(R.id.resetBtn);
 
         View.OnClickListener listener = new OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!gameOngoing) {
+                    Toast.makeText(getApplicationContext(), "Please hit reset to start a new game!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 String name = getResources().getResourceName(v.getId());
                 int ind = name.charAt(name.length() - 1) - '1';
                 int i = ind / 3;
@@ -36,10 +51,18 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                game[i][j] = player ? 2 : 1;
-                buttons[i][j].setImageResource(player ? R.drawable.o : R.drawable.x);
+                ++totalSteps;
+                game[i][j] = player;
+                buttons[i][j].setImageResource(player == 1 ? R.drawable.x : R.drawable.o);
+                if (totalSteps >= 5) {
+                    checkWinner(player, i, j);
+                }
+                player = 3 - player;
+                if (gameOngoing) {
+                    status.setText("Player " + (player == 1 ? "X" : "O")+" playing"+totalSteps);
+                }
 
-                player = !player;
+
             }
         };
 
@@ -53,10 +76,74 @@ public class MainActivity extends AppCompatActivity {
                 buttons[i][j].setOnClickListener(listener);
             }
         }
+        
+    }
 
 
+    private void checkWinner(int player, int i, int j) {
+
+        int cnt = 0;
+        // check horizontal
+        for (int x = 0; x < 3; x++) {
+            if (game[i][x] != player) {
+                break;
+            }
+            ++cnt;
+        }
+
+        if (cnt == 3) {
+            showWinner(player);
+            return;
+        }
+
+        cnt = 0;
+        // check vertical
+        for (int x = 0; x < 3; x++) {
+            if (game[x][j] != player) {
+                break;
+            }
+            ++cnt;
+        }
+
+        if (cnt == 3) {
+            showWinner(player);
+            return;
+        }
+
+        //check diagonal
+
+
+        if (game[1][1] == player &&
+                (game[0][0] == player && game[2][2] == player ||
+                game[0][2] == player && game[2][0] == player)) {
+            showWinner(player);
+            return;
+        }
+
+        if (totalSteps > 8) {
+            gameOngoing = false;
+            status.setText("You tied");
+        }
 
     }
 
+    private void showWinner(int player) {
+        gameOngoing = false;
+        status.setText("Player " + (player == 1 ? "x" : "o") + " won!");
+
+    }
+
+    public void resetGame(View view) {
+        gameOngoing = true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setImageResource(R.drawable.empty);
+                game[i][j] = 0;
+            }
+        }
+        player = 1;
+        totalSteps = 0;
+        status.setText("Player X playing");
+    }
 
 }
